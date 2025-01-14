51b5ad0ace5b425e58dfe6886c42bf85c1dca57f
pankajprasad30 <pankajprasad30@gmail.com>
2025-01-14 20:15:20 +0530

    pushing code to develop branch

---
+++ Let_Us_Python/Python_Basics/Arithmetic_Operations.py
@@ -0,0 +1,13 @@
+a = 4 / 2
+print(a)
+b = 2 * 3
+print(b)
+c = 3 ** 2  # double star means raise to power
+print(c)
+d = 3 // 2  # means quotent
+print(d)
+
+# a = a ** 3
+# print(a)
+a **= 3
+print(a)
